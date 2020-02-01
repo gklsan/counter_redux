c2ee3724b7f438a16df72a964bea6506424ac9f9
Gokul <pgokulmca@gmail.com>
2020-02-01 22:48:50 +0530

    reducer added

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+const initialState = { count: 0 };
+
+export default (state = initialState, action) => {
+	switch(action.type) {
+		case 'INCREASE':
+			return {
+				...state,
+				count: state.count + 1
+			}
+		default: 
+			return state	
+	}
+}
