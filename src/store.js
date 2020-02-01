669ef6c39dfb23e4f356426e037494d72428f287
Gokul <pgokulmca@gmail.com>
2020-02-01 22:49:54 +0530

    store added

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const configureStore = (initialState) => {
+	return createStore(
+		reducer,
+		initialState,
+		composeEnhancers(
+			applyMiddleware(thunk)
+		)
+	)
+}
+
+export default configureStore;
